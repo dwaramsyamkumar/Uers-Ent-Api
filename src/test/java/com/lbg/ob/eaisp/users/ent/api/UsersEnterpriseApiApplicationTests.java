fc8b05f46f771a2cdebe0d341f702b64fef8905b
Naga Advik Reddy <dskkumardsk3@gmail.com>
2025-06-05 11:50:13 +0530

    initial commit

---
+++ src/test/java/com/lbg/ob/eaisp/users/ent/api/UsersEnterpriseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbg.ob.eaisp.users.ent.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersEnterpriseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
